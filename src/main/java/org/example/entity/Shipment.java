7d95edabe097abab034a16520222ff10db5f5892
Viktor Rangelov <viktorrangelov@MacBook-Air.local>
2025-10-22 00:27:56 +0300

    connecting shippment and vehicle and setting up log4j xml

--- src/main/java/org/example/entity/Shipment.java
+++ src/main/java/org/example/entity/Shipment.java
@@ -1,4 +1,34 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+import org.example.enums.ShipingType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="shipment")
 public class Shipment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String startLocation;
+    private String endLocation;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private ShipingType shippingType;
+    private BigDecimal weight;
+    private BigDecimal cost;
+
+    @ManyToOne
+    private Company company;
+
+    @ManyToOne
+    private Employee employee;
+
+    public Shipment() {};
+    public Shipment(Long id) {
+        this.id = id;
+    }
+
 }
--- src/main/java/org/example/entity/Vehicle.java
+++ src/main/java/org/example/entity/Vehicle.java
@@ -1,4 +1,22 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "vehicle")
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    private Company company;
+
+    public Vehicle() {}
+    public Vehicle(Long id, String name, Company company) {
+        this.id = id;
+        this.name = name;
+        this.company = company;
+    }
 }
